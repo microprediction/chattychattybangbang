c0ccbcd9ba89857c4b0738688cb3d099b703e5e3
Peter Cotton <peter.cotton@microprediction.com>
2023-04-11 10:51:10 -0500

    fix creds

--- chattychattybangbang/openaicredentials.py
+++ chattychattybangbang/openaicredentials.py
@@ -2,13 +2,16 @@ import os
 import openai
 
 
-def set_credentials():
-    try:
-        from chattychattybangbang.privatecredentials import API_KEY  # noqa: E402
-        openai.api_key = API_KEY
-    except ImportError:
-        api_key = os.environ.get('OPEN_AI_KEY')
-        if api_key is None:
-            raise Exception('Create privatecredential.py with API_KEY please, or OPEN_AI_KEY env var')
-        else:
+def set_credentials(api_key=None):
+    if api_key is None:
+        try:
+            from chattychattybangbang.privatecredentials import API_KEY  # noqa: E402
             openai.api_key = API_KEY
+        except ImportError:
+            api_key = os.environ.get('OPEN_AI_KEY')
+            if api_key is None:
+                raise Exception('Create privatecredential.py with API_KEY please, or OPEN_AI_KEY env var')
+            else:
+                openai.api_key = api_key
+    else:
+        openai.api_key = api_key
